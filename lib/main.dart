955ceaef71ac09ae73517c1cd1cc230891c53f6b
minyoui <evanlyrush73@gmail.com>
2023-11-09 13:14:19 +0800

    5th commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,14 +24,27 @@ class DicePage extends StatefulWidget {
   State<DicePage> createState() => _DicePageState();
 }
 
-class _DicePageState extends State<DicePage> {
+class _DicePageState extends State<DicePage> with TickerProviderStateMixin {
   int rightDice = Random().nextInt(6) + 1;
   int leftDice = Random().nextInt(6) + 1;
+  double turns = 0.0;
+  int rotationSpeed = 8;
+  int rotationDuration = 3000; // in miliseconds
+
+  bool isJumpscare = false;
 
   void getRandomDiceValues() {
     setState(() {
       rightDice = Random().nextInt(6) + 1;
       leftDice = Random().nextInt(6) + 1;
+
+      isJumpscare = leftDice == rightDice;
+    });
+  }
+
+  void rotateDice() {
+    setState(() {
+      turns += rotationSpeed;
     });
   }
 
@@ -39,9 +52,11 @@ class _DicePageState extends State<DicePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/background.jpg'),
+            image: AssetImage(isJumpscare
+                ? 'images/8bit-pixel5.gif'
+                : 'images/8bit-pixel4.gif'),
           ),
         ),
         child: Scaffold(
@@ -49,33 +64,56 @@ class _DicePageState extends State<DicePage> {
             title: const Text('EA4: Stateless and Stateful Widget'),
             backgroundColor: Colors.black45,
           ),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.black12,
           body: SafeArea(
             child: Center(
-              child: Row(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                          onPressed: () {
-                            getRandomDiceValues();
-                            print('left is clicked');
-                          },
-                          child: Image.asset('images/dice$leftDice.png')),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                            padding: const EdgeInsets.all(50.0),
+                            child: AnimatedRotation(
+                              turns: turns,
+                              curve: Curves.elasticInOut,
+                              duration:
+                                  Duration(milliseconds: rotationDuration),
+                              onEnd: getRandomDiceValues,
+                              child: TextButton(
+                                onPressed: rotateDice,
+                                child: Image.asset('images/dice$leftDice.png'),
+                              ),
+                            )),
+                      ),
+                      Expanded(
+                        child: Padding(
+                            padding: const EdgeInsets.all(50.0),
+                            child: AnimatedRotation(
+                              turns: turns,
+                              curve: Curves.elasticInOut,
+                              duration:
+                                  Duration(milliseconds: rotationDuration),
+                              onEnd: getRandomDiceValues,
+                              child: TextButton(
+                                onPressed: rotateDice,
+                                child: Image.asset('images/dice$rightDice.png'),
+                              ),
+                            )),
+                      ),
+                    ],
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 16)),
+                  Text(
+                    isJumpscare
+                        ? 'YOUR FAITH IS SEALED'
+                        : 'Roll the dice and thy choose your faith',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
                     ),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                          onPressed: () {
-                            getRandomDiceValues();
-                            print('right is clicked');
-                          },
-                          child: Image.asset('images/dice$rightDice.png')),
-                    ),
-                  )
                 ],
               ),
             ),
